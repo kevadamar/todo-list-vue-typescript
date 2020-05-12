ea43562eac25bbb769e148038b021fdd1c52921a
Keva Damar Galih <kevadamarg@gmail.com>
2020-05-12 20:54:42 +0700

    update v3 dist

---
+++ dist/precache-manifest.16f87902a7ddd1b079381f325d207195.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11fb6a4b832374b68285",
+    "url": "/css/Edit.a3fb6984.css"
+  },
+  {
+    "revision": "e00dd6ed83fa4d7c988d",
+    "url": "/css/app.129a6d7c.css"
+  },
+  {
+    "revision": "4c9d1afb8d7ed74c566c",
+    "url": "/css/chunk-vendors.e5eaa337.css"
+  },
+  {
+    "revision": "fdfa9a5f3976f0159674",
+    "url": "/css/home.bb4482a1.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "639b3747bf5be3ec592b9772896d5ea7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11fb6a4b832374b68285",
+    "url": "/js/Edit.6bb01537.js"
+  },
+  {
+    "revision": "e00dd6ed83fa4d7c988d",
+    "url": "/js/app.46ed75e2.js"
+  },
+  {
+    "revision": "4c9d1afb8d7ed74c566c",
+    "url": "/js/chunk-vendors.694d1240.js"
+  },
+  {
+    "revision": "fdfa9a5f3976f0159674",
+    "url": "/js/home.c803a198.js"
+  },
+  {
+    "revision": "a6aafcf548cef8a49c862a9ee7e46b7f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
